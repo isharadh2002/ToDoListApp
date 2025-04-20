0fbe971ce47d0766243dbf9ca1d1c25bea53b47c
Ishara Dhanushan <109136246+isharadh2002@users.noreply.github.com>
2025-04-20 23:02:09 +0530

    Fixing memory issue

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,17 @@
-// App.tsx
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import TaskInput from './components/TaskInput';
 import TaskList from './components/TaskList';
+import {useTaskStore} from './store/taskStore'; // Import the store
 
 export default function Home() {
+  const loadTasks = useTaskStore(state => state.loadTasks); // Get the loadTasks function
+
+  useEffect(() => {
+    // Load tasks when component mounts
+    loadTasks();
+  }, [loadTasks]);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#000" />
